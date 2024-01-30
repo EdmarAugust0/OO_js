a852b05bacb7816dceb9c875b77910cfe4aba183
Edmar <edmar.contato149@gmail.com>
2024-01-30 15:13:59 -0300

    fixed

--- main.js
+++ main.js
@@ -11,15 +11,30 @@ function Funcionario(nome, cargo, salario) {
     }
     
     this.salario = salario;
+}
 
+function petDog(nome, raca, cor, tamanho) {
+    Pessoa.call(this, nome);
+
+    this.raca = raca;
+    this.cor = cor;
+    this.tamanho = tamanho;
 }
 
 const funcionario1 = new Funcionario("Ana Beatriz", "Fisioterapeuta", 3000);
 const funcionario2 = new Funcionario("Edmar", "Dev Front-end", 5000);
 const funcionario3 = new Funcionario("Augusto", "Dev Back-end", 6000);
+
+const petDog1 = new petDog("Ana Beatriz", "pittbul", "preta", "grande");
+const petDog2 = new petDog("Edmar", "yorkshire", "cinza", "pequeno");
+const petDog3 = new petDog("Augusto", "Shih-tzu", "branco", "pequeno");
+
 console.log(funcionario1)
+console.log(petDog1);
 funcionario1.dizCargo();
 
-console.log(funcionario2)
+console.log(funcionario2);
+console.log(petDog2);
 
-console.log(funcionario3)
+console.log(funcionario3);
+console.log(petDog3);
